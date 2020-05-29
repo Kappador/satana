9bcebb788b274fd069834de909c12038f373a063
cnriscute <cnr@ra.pe>
2020-05-29 02:43:19 +0100

    Ammount argument added to "dog" and "cat" command

--- bot/commands/cat.js
+++ bot/commands/cat.js
@@ -17,18 +17,22 @@ module.exports = {
     },
     run: (bot, msg, args) => {
         msg.delete();
+
+        let amm = 1;
         
         if(args[1] && args[1].toLowerCase() == "info") {
             let info = cUtil.getCommand(args[0].replace(prefix, ""));
 
             return msg.channel.send(`\`\`\`asciidoc\nShowing information for: ${args[0].replace(prefix, "")}\n=====\nCommand :: ${info.name}\nDescription :: ${info.description}\nAliases :: ${info.aliases.toString().replace(/\,/g, ", ")}\nPermission :: ${info.permission}\`\`\``);
-        }
+        } else if(args[1] && !isNaN(parseInt(args[1]))) amm = parseInt(args[1]);
 
-        request({
-            uri: "https://api.thecatapi.com/v1/images/search",
-            json: true
-        }).then(data => {
-            msg.channel.send({files: [{attachment: data[0].url, "name": `kitty.${data[0].url.split(".")[3]}`}]});
-        });
+        for(let i = 0; i < amm; i++) {
+            request({
+                uri: "https://api.thecatapi.com/v1/images/search",
+                json: true
+            }).then(data => {
+                msg.channel.send({files: [{attachment: data[0].url, "name": `kitty.${data[0].url.split(".")[3]}`}]});
+            });
+        }
     }
 }
