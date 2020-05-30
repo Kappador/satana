903da01773a79a1eba8812f80fc946bb0430da7d
cnriscute <cnr@ra.pe>
2020-05-30 21:32:24 +0100

    Finished checkhost command

--- bot/commands/checkhost.js
+++ bot/commands/checkhost.js
@@ -24,6 +24,26 @@ module.exports = {
             let info = cUtil.getCommand(args[0].replace(prefix, ""));
 
             return msg.channel.send(`\`\`\`asciidoc\nShowing information for: ${args[0].replace(prefix, "")}\n=====\nCommand :: ${info.name}\nDescription :: ${info.description}\nAliases :: ${info.aliases.toString().replace(/\,/g, ", ")}\nPermission :: ${info.permission}\`\`\``);
+        } else if(args[1] && args[1].toLowerCase() != "info") {
+            request({
+                uri: `https://check-host.net/check-http?host=${args[1]}&max_nodes=3`,
+                json: true
+            }).then(data => {
+                let nodes = Object.keys(data.nodes);
+                let message  = `\`\`\`asciidoc\nSUCCESS!\n=====\n= Results for ${args[1]} =\n`;
+            
+                for(let i = 0; i < nodes.length; i++) {
+                    let node = data.nodes[nodes[i]];
+                    let country = node[1];
+                    let status = (typeof node[5] == "undefined" ? "NONE" : node[5]);
+                    
+                    message += `${country} :: Status: ${status}\n`
+                }
+            
+                message += `\`\`\``;
+            
+                msg.channel.send(message);
+            });
         }
     }
 }
