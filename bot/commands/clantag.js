b7a3f4abb5b422868578d383c70dc9c7bcd505c1
conner <weaboosfun@gmail.com>
2020-06-02 03:35:11 +0100

    Fix me Kappa (thanks sexy)

--- bot/commands/clantag.js
+++ bot/commands/clantag.js
@@ -23,98 +23,59 @@ module.exports = {
         let kappa = ["$£_.-$0-1'", "k£_.-$0-1'", "ka_.-$0-1'", "kap.-$0-1'", "kapp-$0-1'", "kappa$0-1'", "kappa.0-1'", "kappa.h-1'", "kappa.ho1'", "kappa.hos'", "kappa.host"];
         let host = ["p", "p.", "pa", "pa.", "pay", "pay.", "pays", "pays.", "pays.", "pays.h", "pays.h.", "pays.ho", "pays.ho.", "pays.hos", "pays.hos.", "pays.host"];
         let otc = ["onetap.su", "netap.su o", "etap.su on", "tap.su one", "ap.su onet", "p.su oneta", ".su onetap", "su onetap.", "u onetap.", "onetap.s"];
+        
+        let ct;
 
         if(args[0]) {
             let tag = args[0];
+            let x = 0;
 
             switch(tag) {
-                case "satana": 
-                    function satanaTag() {
-                        let x = 0;
-                        for(let i = 0; i < satana.length; i++) {
-                            setTimeout(() => {
-                                if(stop) return;
+                case "satana":
+                    ct = setInterval(() => {
+                        dUtil.setStatus(satana[x], null, token);
+                        x++;
 
-                                dUtil.setStatus(satana[x], null, token);
-
-                                x++;
-                                if(x == satana.length) setTimeout(() => {
-                                    satanaTag();
-                                }, 2500);
-                            }, i * 2500);
-                        }
-                    }
-
-                    satanaTag();
+                        if(x == satana.length) x = 0;
+                    }, 2500);
 
                     msg.channel.send(`\`\`\`asciidoc\nSUCCESS!\n=====\nSuccess :: Clantag has been set to SATANA\n\`\`\``);
                     break;
                 case "kappa":
-                    function kappaTag() {
-                        let x = 0;
-                        for(let i = 0; i < kappa.length; i++) {
-                            setTimeout(() => {
-                                if(stop) return;
+                    ct = setInterval(() => {
+                        dUtil.setStatus(kappa[x], null, token);
+                        x++;
 
-                                dUtil.setStatus(kappa[x], null, token);
-
-                                x++;
-                                if(x == kappa.length) setTimeout(() => {
-                                    kappaTag();
-                                }, 2500);
-                            }, i * 2500);
-                        }
-                    }
-
-                    kappaTag();
+                        if(x == kappa.length) x = 0;
+                    }, 2500);
 
                     msg.channel.send(`\`\`\`asciidoc\nSUCCESS!\n=====\nSuccess :: Clantag has been set to KAPPA\n\`\`\``);
                     break;
                 case "host":
-                    function hostTag() {
-                        let x = 0;
-                        for(let i = 0; i < host.length; i++) {
-                            setTimeout(() => {
-                                if(stop) return;
+                    ct = setInterval(() => {
+                        dUtil.setStatus(host[x], null, token);
+                        x++;
 
-                                dUtil.setStatus(host[x], null, token);
-
-                                x++;
-                                if(x == host.length) setTimeout(() => {
-                                    hostTag();
-                                }, 2500);
-                            }, i * 2500);
-                        }
-                    }
-
-                    hostTag();
+                        if(x == host.length) x = 0;
+                    }, 2500);
 
                     msg.channel.send(`\`\`\`asciidoc\nSUCCESS!\n=====\nSuccess :: Clantag has been set to HOST\n\`\`\``);
                     break;
                 case "otc":
                     // Thanks Quake =)
-                    function otcTag() {
-                        let x = 0;
-                        for(let i = 0; i < otc.length; i++) {
-                            setTimeout(() => {
-                                if(stop) return;
+                    ct = setInterval(() => {
+                        dUtil.setStatus(otc[x], null, token);
+                        x++;
 
-                                dUtil.setStatus(otc[x], null, token);
-
-                                x++;
-                                if(x == otc.length) setTimeout(() => {
-                                    otcTag();
-                                }, 2500);
-                            }, i * 2500);
-                        }
-                    }
-
-                    otcTag();
+                        if(x == otc.length) x = 0;
+                    }, 2500);
 
                     msg.channel.send(`\`\`\`asciidoc\nSUCCESS!\n=====\nSuccess :: Clantag has been set to OTC\n\`\`\``);
                     break;
                 case "stop":
-                    stop = true;
+                    console.log(ct); // undefined
+                    clearInterval(ct);
+                    x = 0;
 
                     msg.channel.send(`\`\`\`asciidoc\nSUCCESS!\n=====\nSuccess :: Stopped clantag\n\`\`\``);
                     break;
